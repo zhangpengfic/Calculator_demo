a5a0f21b9b3a8283f3e71a6ce291d1883325679e
Alfred Zhang <alfred@macxdemacbook-pro-7.local>
2016-06-12 14:49:35 +0800

    finish the l5

--- Calculator_demo/CalculatorBrain.swift
+++ Calculator_demo/CalculatorBrain.swift
@@ -46,6 +46,29 @@ class CalculatorBrain {
     }
     
     
+    var program : AnyObject{
+        get{
+            return opStack.map{$0.description}
+        }
+        set{
+            if let opSymbols = newValue as? Array<String> {
+                var newOpStack = [Op]()
+                for opSymbol in opSymbols {
+                    if let op=knownOps[opSymbol] {
+                        newOpStack.append(op)
+                    }else if let operand = NSNumberFormatter().numberFromString(opSymbol)?.doubleValue {
+                        newOpStack.append(Op.Operand(operand))
+                    }
+                    
+                }
+                opStack = newOpStack
+            }
+        }
+        
+        
+    }
+    
+    
     func evaluate(ops:[Op]) -> (result:Double?, remainingOps:[Op]) {
         if !ops.isEmpty {
             var remainingOps = ops
