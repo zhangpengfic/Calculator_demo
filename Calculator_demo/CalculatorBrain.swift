515b98233dd97a153ff62c0a0e171df7a9c3fdc5
Alfred <zhangpeng.fic@163.com>
2016-07-28 16:58:21 +0800

    step4, UnaryOperation

--- Calculator_demo/CalculatorBrain.swift
+++ Calculator_demo/CalculatorBrain.swift
@@ -16,17 +16,26 @@ class CalculatorBrain {
         accumulater=operand
     }
     var operations:Dictionary<String,Operation> =  [
-        "℮":Operation.Constant(M_E)
+        "∏":Operation.Constant(M_PI),
+        "℮":Operation.Constant(M_E),
+        "√":Operation.UnaryOperation(sqrt),
+        "cos":Operation.UnaryOperation(cos)
     ]
     
     enum Operation {
         case Constant(Double)
+        case UnaryOperation(Double->Double)
+        case BinaryOperation
+        case Equals
     }
     
     func performOperation(symbol:String) {
         if let constant = operations[symbol] {
             switch constant {
-            case .Constant(let value):accumulater = value
+            case.Constant(let value):accumulater = value
+            case.UnaryOperation(let function):accumulater = function(accumulater)
+            case.BinaryOperation:break
+            case.Equals:break
             }
             
         }
